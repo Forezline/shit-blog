f6b80d28090fcaa28ee37fe6b316cd4f8911ebb0
Forezline <135609192+Forezline@users.noreply.github.com>
2025-05-07 14:13:27 +0300

    Delete blog directory

--- blog/includes/mail.php
+++
@@ -1,9 +0,0 @@
-<?php
-function sendResetEmail($to, $reset_link) {
-    $subject = "Скидання паролю для вашого акаунта";
-    $message = "Щоб скинути пароль, натисніть на наступне посилання: $reset_link";
-    $headers = "From: no-reply@yourdomain.com";
-
-    mail($to, $subject, $message, $headers);
-}
-?>
